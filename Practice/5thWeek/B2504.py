d673fd6472fcd15fa82aad215e97d21c91528303
unknown <zxskmj@gmail.com>
2023-02-13 11:30:33 +0900

    0213_modify

--- Practice/5thWeek/B2504.py
+++ Practice/5thWeek/B2504.py
@@ -1,9 +1,37 @@
 s = input()
-N = len(s)
+length = len(s)
 
-small = 0           # 소괄호
-big = 0             # 대괄호
-for i in range(N):
-    s = s.replace('()', '2')
-    s = s.replace('[]', '3')
-    
+stack = []
+temp = 1
+res = 0
+for i in range(length):
+    # 열린 괄호, 누적하여 계산됨
+    brk = s[i]  # bracket 줄임
+    if brk == '(':
+        temp *= 2
+        stack.append(brk)
+    elif brk == '[':
+        temp *= 3
+        stack.append(brk)
+
+    # 괄호 닫을 때, 계산 진행
+    elif brk == ')':
+        if not stack or stack[-1] == '[':
+            res = 0
+            break
+        if s[i-1] == '(':
+            res += temp
+        temp //= 2
+        stack.pop()
+    elif brk == ']':
+        if not stack or stack[-1] == '(':
+            res = 0
+            break
+        if s[i-1] == '[':
+            res += temp
+        temp //= 3
+        stack.pop()
+
+if stack:
+    res = 0
+print(res)
