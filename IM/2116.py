a48957fa2ccd0a425712d8556f5ec62b3b29515f
MJ Kim <zxskmj@gmail.com>
2023-02-19 22:13:27 +0900

    0219_IM

--- IM/2116.py
+++ IM/2116.py
@@ -12,32 +12,24 @@ for i in range(1, 7):   # 1~6 까지의 수
     num = i
     dice_sum = 0
     for j in range(N):  # 주사위 갯수
-        print(num, end=':')
         if num == dice[j][0]:
             num = dice[j][1]
             dice_sum += max(dice[j][2:])
-            print(dice[j][2:], end=' ')
         elif num == dice[j][1]:
             num = dice[j][0]
             dice_sum += max(dice[j][2:])
-            print(dice[j][2:], end=' ')
         elif num == dice[j][2]:
             num = dice[j][3]
-            dice_sum += max(max(dice[j][:2], dice[j][4:]))
-            print(dice[j][:2], dice[j][4:], end=' ')
+            dice_sum += max(max(dice[j][:2]), max(dice[j][4:]))
         elif num == dice[j][3]:
             num = dice[j][2]
-            dice_sum += max(max(dice[j][:2], dice[j][4:]))
-            print(dice[j][:2], dice[j][4:], end=' ')
+            dice_sum += max(max(dice[j][:2]), max(dice[j][4:]))
         elif num == dice[j][4]:
             num = dice[j][5]
             dice_sum += max(dice[j][:4])
-            print(dice[j][:4], end=' ')
         elif num == dice[j][5]:
             num = dice[j][4]
             dice_sum += max(dice[j][:4])
-            print(dice[j][:4], end=' ')
-    print()
     res = max(res, dice_sum)
 
 print(res)
