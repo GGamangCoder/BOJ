12f408b3b03abceef2be64eeeeb4dd218b666c37
unknown <zxskmj@gmail.com>
2023-02-24 17:51:32 +0900

    0224_simul_20056

--- Simulation/20056.py
+++ Simulation/20056.py
@@ -13,24 +13,66 @@
 
 # K 번 이동 -> ans; 남은 질량 합
 
-
-N, M, K = map(int, input().split())     # 크기, 갯수, 이동 횟수
+# 크기, 갯수, 이동 횟수
+N, M, K = map(int, input().split())
 graph = [[[] for _ in range(N)] for _ in range(N)]
-print(graph)
-# 방향
+# 방향 (0, 1, 2, 3, 4, 5, 6, 7)
 d = [(0, -1), (1, -1), (1, 0), (1, 1), (0, -1), (-1, 1), (-1, 0), (-1, -1)]
+# 그래프에 현재 파이어볼 상태 담아주기
 for i in range(M):
     row, col, mass, vel, dir =\
         map(int, input().split())
     graph[row-1][col-1].append([mass, vel, dir])
-print(graph)
-######## 입력 끝
 # K번의 이동 동작 수행
 def moving():
-    pass
+    global graph
+    arr = [[[] for _ in range(N)] for _ in range(N)]
+    # 모든 좌표들에 대해서
+    for i in range(N):
+        for j in range(N):
+            for k in range(len(graph[i][j])):
+                ni = (i + d[graph[i][j][k][2]][0] * graph[i][j][k][1]) % N
+                nj = (j + d[graph[i][j][k][2]][0] * graph[i][j][k][1]) % N
+                # 해당 정보는 그대로 넘어가기
+                arr[ni][nj].append([graph[i][j][k]])
+    graph = arr     # 다시 덧입혀주기
 
 # 합쳐졌다가 분해되는 것에 대한 계산
-def seperate():
-    pass
+# arr = graph[row][col] # 타입은 []
+def seperate(arr):      # 특정 좌표에 담겨있는 리스트
+    cnt = len(arr)      # 합쳐지는 파이어볼 갯수
+    m = v = temp1 = temp2 = 0     # 질량, 속력, 홀/짝
+    for i in range(cnt):
+        m += arr[i][0]
+        v += arr[i][1]
+        # 방향에 대한 조사
+        if arr[i][2] % 2 == 0:      # 짝수
+            temp2 += 1
+        else:
+            temp1 += 1
+    m //= 5
+    if m == 0:
+        return
+    v //= cnt
+    if temp1 == cnt or temp2 == cnt:        # 모두 홀/짝
+        new_arr = [[m, v, i] for i in range(0, 8, 2)]
+    else:
+        new_arr = [[m, v, i] for i in range(1, 8, 2)]
+    return new_arr
 
 # 이동 후에 혹시 겹치는 부분 있는지 체크해주는 부분
+for _ in range(K):
+    moving()
+    for i in range(N):
+        for j in range(N):
+            if len(graph[i][j]) > 1:
+                graph[i][j] = seperate(graph[i][j])
+
+print(graph)
+res = 0
+for i in range(N):
+    for j in range(N):
+        print(graph[i][j])
+        if graph[i][j]:
+            res += graph[i][j][0]
+print(res)
