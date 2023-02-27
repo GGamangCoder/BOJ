b6a851e78a3946e66bad74e799e3951ed87ca727
unknown <zxskmj@gmail.com>
2023-02-27 16:31:47 +0900

    0227_2331_math

--- Math/2331.py
+++ Math/2331.py
@@ -1,11 +1,27 @@
 # 반복 수열
 # 수학, 구현
 
-n = 5
-print(n << 3)
+dp = []
 
-n = 0.625
-print(type(n))
+# str
+A, P = input().split()
+dp = [A]
 
-n = n << 3
-print(n)
+def sum(num):
+    ans = 0
+    for i in num:
+        ans += int(i)**int(P)
+    return str(ans)
+
+res = 0
+idx = 0
+while True:
+    dp_next = sum(dp[idx])
+    if dp_next in dp:
+        res = dp.index(dp_next)
+        break
+    else:
+        dp.append(dp_next)
+        idx += 1
+
+print(res)
