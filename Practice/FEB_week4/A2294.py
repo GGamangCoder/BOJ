929daccbbe88747b866ffcc7e21f89fcafb378a7
MJ Kim <zxskmj@gmail.com>
2023-03-09 02:03:08 +0900

    feb_week4_2294

--- Practice/FEB_week4/A2294.py
+++ Practice/FEB_week4/A2294.py
@@ -2,11 +2,24 @@
 
 # n가지의 동전 -> k원
 # 결과는 가능한 최소 갯수, 불가능 -1
-n, k = map(int, input().split())
-val = set()
-for _ in range(n):
-    val.add(int(input()))
-val = sorted(val)
-dp = [0] * (k+1)        # 1부터 가치 k까지 빈 배열
+import sys
+input = sys.stdin.readline
 
-# 1부터 차례대로 가치 창출
+n, k = map(int, input().split())
+coins = [0] * n
+for i in range(n):
+    coins[i] = int(input().strip())
+
+# 경우의 수를 담는 dp 생성, 밑에서 최솟값 갱신하므로 최대값으로 초기화
+dp = [10001] * (k+1)
+# dp[0] = 0
+# 모든 코인 경우에 따라서
+for coin in coins:
+    for i in range(coin, k+1):
+        # 새로운 것이 그 코인을 포함하는지에 따라 최솟값 갱신
+        dp[i] = min(dp[i], dp[i-coin]+1)
+
+if dp[k] == 10001:
+    print(-1)
+else:
+    print(dp[k])
