89dcf83999449030db2d4ec1b6c528b36a0dd61a
MJ Kim <zxskmj@gmail.com>
2023-03-20 22:27:11 +0900

    CT_week2_D_8911

--- Practice/CT_week2/D_8911.py
+++ Practice/CT_week2/D_8911.py
@@ -7,9 +7,10 @@ input = sys.stdin.readline
 
 
 # 영역을 계산해주는 함수
+# direction = [y, x, -y, -x]
 def solve(cmd):
     direction = lx = rx = ty = by = 0       # 현재 방향, 왼/오, 위/아래
-    x = y = 0
+    x = y = 0       # 변하는 값
     # 명령 받는대로
     for s in cmd:
         if s == 'L':
@@ -19,13 +20,40 @@ def solve(cmd):
 
         # 현재 방향대로 전진 혹은 후진
         elif s == 'F':
-            pass
+            if direction % 4 == 0:
+                y += 1
+                ty = max(y, ty)
+            elif direction % 4 == 1:
+                x += 1
+                rx = max(x, rx)
+            elif direction % 4 == 2:
+                y -= 1
+                by = min(y, by)
+            elif direction % 4 == 3:
+                x -= 1
+                lx = min(x, lx)
         elif s == 'B':
-            pass
+            if direction % 4 == 0:
+                y -= 1
+                by = min(y, by)
+            elif direction % 4 == 1:
+                x -= 1
+                lx = min(x, lx)
+            elif direction % 4 == 2:
+                y += 1
+                ty = max(y, ty)
+            elif direction % 4 == 3:
+                x += 1
+                rx = max(x, rx)
+        # print(x, y, rx, lx, ty, by, '&', direction)
 
+    length = rx - lx
+    width = ty - by
+    return length * width
 
 
 T = int(input())
 for tc in range(1, T+1):
     command = input()
+    
     print(solve(command))
