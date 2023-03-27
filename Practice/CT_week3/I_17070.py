daf1dc79c9795c37d8402c55d97181b847a04c33
MJ Kim <zxskmj@gmail.com>
2023-03-28 00:29:28 +0900

    CT_week3_I_17070

---
+++ Practice/CT_week3/I_17070.py
@@ -0,0 +1,36 @@
+# 파이프 옮기기 1
+
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+N = int(input())
+graph = [list(map(int, input().split())) for _ in range(N)]
+
+cnt = 0
+# before 에 따라서 가로 세로 대각선
+# before = 0(가로), 1(세로), 2(대각선)
+def dfs(x, y, before):
+    global cnt
+    if x == N-1 and y == N-1:
+        cnt += 1
+        return
+    
+    # 대각선 이동
+    if x + 1 < N and y + 1 < N:
+        if not graph[y+1][x+1] and not graph[y+1][x] and not graph[y][x+1]:
+            dfs(x+1, y+1, 2)
+
+    # 가로 이동
+    if before != 1 and x + 1 < N:
+        if not graph[y][x+1]:
+            dfs(x+1, y, 0)
+
+    # 세로 이동
+    if before != 0 and y + 1 < N:
+        if not graph[y+1][x]:
+            dfs(x, y+1, 1)
+
+# (0, 0) -> (1, 0) / 가로 방향 시작(0)
+dfs(1, 0, 0)
+print(cnt)
