6aff67af2267eb1a381a69c0ceebb45fa7d0cff4
MJ Kim <zxskmj@gmail.com>
2023-04-16 16:09:49 +0900

    solve graph/13549 BF-backtracking

--- Graph/10971.py
+++ Graph/10971.py
@@ -1,2 +1,37 @@
-# 외판원 순회 2
+# 외판원 순회 2(TSP)
 # BF, 백트래킹
+
+import sys
+input = sys.stdin.readline
+
+def dfs(start, end, sumValue, cnt):
+    global minValue
+    # 모든 순회 끝내고 돌아갈 때 체크
+    if cnt == n:
+        if graph[end][start]:
+            sumValue += graph[end][start]
+            if minValue > sumValue:
+                minValue = sumValue
+        return
+
+    if minValue < sumValue:
+        return
+
+    for next in range(n):
+        if not visited[next] and graph[end][next]:
+            visited[next] = 1
+            dfs(start, next, sumValue + graph[end][next], cnt+1)
+            visited[next] = 0
+
+
+n = int(input())
+graph = [list(map(int, input().split())) for _ in range(n)]
+minValue = sys.maxsize
+
+visited = [0] * n
+for i in range(n):
+    visited[i] = 1
+    dfs(i, i, 0, 1)
+    visited[i] = 0
+
+print(minValue)
